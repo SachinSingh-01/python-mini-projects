0bc08da9b701bc377ef2885a9f65b409c731092a
Sachin Singh <sachinsingh19012@gmail.com>
2026-02-09 20:41:07 +0530

    Mini Chat Logger

---
+++ 09_problem.py
@@ -0,0 +1,34 @@
+'''Mini Chat Logger
+Save chat messages.
+Requirements
+User enters messages repeatedly
+Type "exit" to stop
+Store all messages in list
+Write all messages into chatlog.txt
+Read file and display again
+Concepts used
+list
+loop
+strings
+file handling
+functions
+'''
+message=[]
+def text_message():
+    while True:
+        text=input("Enter the text:")
+        if text=="exit":
+            print("You stop")
+            break
+        message.append(text)
+def save_message():
+    with open("chatlog.txt","a") as file:
+        for meg in message:
+            file.write(meg +"\n")
+def show_result():
+    with open("chatlog.txt","r") as file:
+        print("chat log \n")
+        print(file.read())
+text_message()
+save_message()
+show_result()
